f7b76a5029ff3469255a9083c31e37c845ba4c90
Lior Lahav <LahavLior@gmail.com>
2020-07-06 11:44:38 +0300

    Fixed: Winform mouse click events

--- NetSettingsCore.WinForms/WinFormCheckBox.cs
+++ NetSettingsCore.WinForms/WinFormCheckBox.cs
@@ -7,9 +7,20 @@ namespace NetSettings.Forms
 {
     public class WinFormCheckBox : CheckBox, ICheckBox
     {
+        public WinFormCheckBox()
+        {
+            base.MouseClick += WinFormCheckBox_MouseClick;
+        }
         public new IList Controls => base.Controls;
         public IFont Font { get; set; }
         public event EventHandler MouseClick;
+
+        private void WinFormCheckBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            MouseClick(sender, e as EventArgs);
+        }
+
+        //public event EventHandler MouseClick { get; set; }// { return CheckBox.MouseClick; };
         public event EventHandler SelectedIndexChanged;
         public event EventHandler MouseDoubleClick;
         public event EventHandler KeyDown;
