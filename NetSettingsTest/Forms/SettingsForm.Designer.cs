a122facc419398bf8a63d51fb486acd4f1f39adb
OG <O@G>
2020-07-11 23:34:21 +0300

    Everything compiles

--- NetSettingsTest/Forms/SettingsForm.Designer.cs
+++ NetSettingsTest/Forms/SettingsForm.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace NetSettings.Forms
+﻿using System.Drawing;
+using NetSettingsTestCore.Controls;
+
+namespace NetSettings.Forms
 {
     partial class SettingsForm
     {
@@ -33,8 +36,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
-            this.controlContainer1 = new NetSettings.Controls.ControlContainer();
-            this.controlContainer2 = new NetSettings.Controls.ControlContainer();
+            this.controlContainer1 = new ControlContainer();
+            this.controlContainer2 = new ControlContainer();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -47,11 +50,9 @@
             // 
             // splitContainer1
             // 
-            this.splitContainer1.Cursor = System.Windows.Forms.Cursors.HSplit;
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.splitContainer1.Location = new Point(0, 0);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -64,18 +65,16 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
-            this.splitContainer1.Size = new System.Drawing.Size(529, 883);
-            this.splitContainer1.SplitterDistance = 61;
-            this.splitContainer1.SplitterWidth = 6;
+            this.splitContainer1.Size = new System.Drawing.Size(397, 574);
+            this.splitContainer1.SplitterDistance = 38;
             this.splitContainer1.TabIndex = 0;
             // 
             // btnSave
             // 
             this.btnSave.Enabled = false;
-            this.btnSave.Location = new System.Drawing.Point(401, 15);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSave.Location = new Point(301, 10);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(100, 35);
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
             this.btnSave.TabIndex = 3;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -84,19 +83,17 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 23);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new Point(22, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 20);
+            this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Filter:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(93, 18);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Location = new Point(70, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 27);
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -104,8 +101,7 @@
             // 
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
-            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.splitContainer2.Location = new Point(0, 0);
             this.splitContainer2.Name = "splitContainer2";
             this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -116,19 +112,17 @@
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this.controlContainer2);
-            this.splitContainer2.Size = new System.Drawing.Size(529, 816);
-            this.splitContainer2.SplitterDistance = 693;
-            this.splitContainer2.SplitterWidth = 6;
+            this.splitContainer2.Size = new System.Drawing.Size(397, 532);
+            this.splitContainer2.SplitterDistance = 411;
             this.splitContainer2.TabIndex = 0;
             // 
             // controlContainer1
             // 
             this.controlContainer1.AutoScroll = true;
             this.controlContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.controlContainer1.Location = new System.Drawing.Point(0, 0);
-            this.controlContainer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.controlContainer1.Location = new Point(0, 0);
             this.controlContainer1.Name = "controlContainer1";
-            this.controlContainer1.Size = new System.Drawing.Size(529, 693);
+            this.controlContainer1.Size = new System.Drawing.Size(397, 411);
             this.controlContainer1.TabIndex = 0;
             this.controlContainer1.Text = "controlContainer1";
             // 
@@ -136,21 +130,19 @@
             // 
             this.controlContainer2.AutoScroll = true;
             this.controlContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.controlContainer2.Location = new System.Drawing.Point(0, 0);
-            this.controlContainer2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.controlContainer2.Location = new Point(0, 0);
             this.controlContainer2.Name = "controlContainer2";
-            this.controlContainer2.Size = new System.Drawing.Size(529, 117);
+            this.controlContainer2.Size = new System.Drawing.Size(397, 117);
             this.controlContainer2.TabIndex = 0;
             this.controlContainer2.Text = "controlContainer2";
             // 
             // SettingsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(529, 883);
+            this.ClientSize = new System.Drawing.Size(397, 574);
             this.Controls.Add(this.splitContainer1);
             this.KeyPreview = true;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "SettingsForm";
             this.Text = "SettingsForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SettingsForm_FormClosing);
@@ -173,8 +165,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.SplitContainer splitContainer2;
-        private NetSettings.Controls.ControlContainer controlContainer1;
-        private NetSettings.Controls.ControlContainer controlContainer2;
+        private ControlContainer controlContainer1;
+        private ControlContainer controlContainer2;
         private System.Windows.Forms.Button btnSave;
     }
 }
