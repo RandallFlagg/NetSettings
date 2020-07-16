fa3bf177e046bceb9fbb168772606d5a7115b90c
Lior Lahav <LahavLior@gmail.com>
2020-07-16 15:22:55 +0300

    Fixed: DockStyle by introducing GUIBindings for Winforms

--- NetSettingsCore.WinForms/WinFormControls/WinFormTextBox.cs
+++ NetSettingsCore.WinForms/WinFormControls/WinFormTextBox.cs
@@ -1,48 +1,88 @@
 ﻿using System;
+using System.Collections;
+using System.Drawing;
 using System.Windows.Forms;
-using NetSettings.Common.Interfaces;
-using BorderStyle = NetSettings.Common.Classes.BorderStyle;
-using WinFormBorderStyle = System.Windows.Forms.BorderStyle;
+using NetSettingsCore.Common;
+using BorderStyle = NetSettingsCore.Common.BorderStyle;
+using DockStyle = NetSettingsCore.Common.DockStyle;
 
-namespace NetSettings.WinForms.WinFormControls
+namespace NetSettingsCore.WinForms.WinFormControls
 {
-    internal class WinFormTextBox : WinFormControl, ITextBox
+    public class WinFormTextBox : TextBox, ITextBox
     {
-        private readonly TextBox _textBox = new TextBox();
-
-        public WinFormTextBox()
+        private WinFormFont _winFormFont;
+        public new IList Controls => base.Controls;
+        public new IFont Font
         {
-            _control = _textBox;
+            get
+            {
+                if (_winFormFont == null)
+                {
+                    _winFormFont = new WinFormFont(base.Font);
+                }
+
+                return _winFormFont;
+
+            }
+            set
+            {
+                base.Font = new Font(value.FontFamily, value.Size, (FontStyle)value.Appearance);
+                _winFormFont = new WinFormFont(base.Font);
+            }
         }
 
-        public bool Multiline { get => _textBox.Multiline; set => _textBox.Multiline = value; }
-        public bool ReadOnly { get => _textBox.ReadOnly; set => _textBox.ReadOnly = value; }
-
-        public BorderStyle BorderStyle
+        public event EventHandler MouseClick;
+        public event EventHandler SelectedIndexChanged;
+        public event EventHandler MouseDoubleClick;
+        public event EventHandler KeyDown;
+        public ITextBox Instance { get=>this; }
+        public DockStyle Dock 
         {
-            get => Enum.Parse<BorderStyle>(_textBox.BorderStyle.ToString());
-            set => Enum.Parse<WinFormBorderStyle>(value.ToString());
+            get { return GUIBindings.FromWinFormDockStyle(base.Dock);}
+            set { base.Dock = GUIBindings.ToWinFormDockStyle(value); }
+        }
+        public BorderStyle BorderStyle { get; set; }
+    }
+
+    class GUIBindings
+    {
+        public static DockStyle FromWinFormDockStyle(System.Windows.Forms.DockStyle dockStyle)
+        {
+            switch (dockStyle)
+            {
+                case System.Windows.Forms.DockStyle.None:
+                    return DockStyle.None;
+                    break;
+                case System.Windows.Forms.DockStyle.Top:
+                    break;
+                case System.Windows.Forms.DockStyle.Bottom:
+                    break;
+                case System.Windows.Forms.DockStyle.Left:
+                    break;
+                case System.Windows.Forms.DockStyle.Right:
+                    break;
+                case System.Windows.Forms.DockStyle.Fill:
+                    return DockStyle.Fill;
+                    break;
+            }
+
+            return DockStyle.None;
         }
 
-        //private WinFormFont _winFormFont;
+        public static System.Windows.Forms.DockStyle ToWinFormDockStyle(DockStyle dockStyle)
+        {
+            switch (dockStyle)
+            {
+                case DockStyle.None:
+                    return System.Windows.Forms.DockStyle.None;
+                case DockStyle.Fill:
+                    return System.Windows.Forms.DockStyle.Fill;
+                    
+            }
+            return System.Windows.Forms.DockStyle.None;
 
-        //public new IFont Font
-        //{
-        //    get
-        //    {
-        //        if (_winFormFont == null)
-        //        {
-        //            _winFormFont = new WinFormFont(base.Font);
-        //        }
 
-        //        return _winFormFont;
+        }
 
-        //    }
-        //    set
-        //    {
-        //        base.Font = new Font(value.FontFamily, value.Size, (FontStyle)value.Appearance);
-        //        _winFormFont = new WinFormFont(base.Font);
-        //    }
-        //}
     }
 }
