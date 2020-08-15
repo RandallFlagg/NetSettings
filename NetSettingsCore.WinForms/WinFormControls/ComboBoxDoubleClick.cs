d35f828a0c25d884b97fa85c2978d3cff31e6691
OG <O@G>
2020-08-15 20:45:25 +0300

    Rendering works

--- NetSettingsCore.WinForms/WinFormControls/ComboBoxDoubleClick.cs
+++ NetSettingsCore.WinForms/WinFormControls/ComboBoxDoubleClick.cs
@@ -5,22 +5,25 @@ namespace NetSettingsCore.WinForms.WinFormControls
 {
     //TODO: Can this be deleted? See WinFormLabel
     //TODO: Can this be changed to internal?
-    public class ComboBoxDoubleClick : WinFormComboBox 
+    internal class ComboBoxDoubleClick : WinFormComboBox 
     {
         DateTime prevClick = DateTime.MinValue;
 
-        protected override void OnMouseClick(MouseEventArgs e)
+        public ComboBoxDoubleClick()
         {
-            this.DroppedDown = true;
-            if (DateTime.Now.AddMilliseconds(-500) < prevClick)
-            {
-                prevClick = DateTime.MinValue;
-                OnMouseDoubleClick(e);
-            }
-            else
-                prevClick = DateTime.Now;
-
-            base.OnMouseClick(e);
+            throw new NotImplementedException("Do we need the event below?");
         }
+        //protected override void OnMouseClick(MouseEventArgs e)
+        //{
+        //    if (DateTime.Now.AddMilliseconds(-500) < prevClick)
+        //    {
+        //        prevClick = DateTime.MinValue;
+        //        OnMouseDoubleClick(e);
+        //    }
+        //    else
+        //        prevClick = DateTime.Now;
+
+        //    base.OnMouseClick(e);
+        //}
     }
 }
